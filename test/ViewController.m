b425ab576d81b79ce95aee683240f97b2b14886e
Scott Kaiser <scott.kaiser@blinker.com>
2016-03-07 10:54:27 -0700

    Test to see if navigation bar navigation items prevent top area of button from being tappable.

--- test/ViewController.m
+++ test/ViewController.m
@@ -18,6 +18,9 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)test:(id)sender {
+  printf("its working!!!");
+}
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
